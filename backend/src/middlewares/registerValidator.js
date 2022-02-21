16a262d02c2dd5f07bdeb958ee6f7f83fb78e7d8
JoaquinGorosito <joaco.gorositoa@gmail.com>
2022-02-21 14:34:04 -0300

    subiendo validaciones de registro

--- backend/src/middlewares/registerValidator.js
+++ backend/src/middlewares/registerValidator.js
@@ -1,5 +1,16 @@
 const { body } = require("express-validator");
-const User = require("../database/models");
+const { compare } = require("bcrypt");
+const db = require("../database/models");
+
 
 module.exports = [
-];
+    body('name').notEmpty().withMessage('Nombre requerido').bail(),
+    body('email').notEmpty().withMessage('email requerido').isEmail().withMessage('email invalido').bail().custom(async (value) => { 
+        const email = await db.User.findOne({ email: value }); 
+        // Buscamos el VALUE enviado del formulario de register en la BD y en caso de que sea falso retornamos un mensaje
+        return !email && Promise.reject("Email existente");
+      }),
+    body('pass').notEmpty()
+    .withMessage("Contraseña requerida")
+    .bail().isLength({min:6}).withMessage('La contraseña debe tener un mínimo de 6 caracteres')
+]
