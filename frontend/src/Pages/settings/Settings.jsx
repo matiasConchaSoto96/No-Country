ef4225bb9829bb044c7ae372ea132a70b0ee6134
Luis Escalona <89802245+LuiscEscalona@users.noreply.github.com>
2022-03-08 19:41:39 -0300

    Add svg

--- frontend/src/Pages/settings/Settings.jsx
+++ frontend/src/Pages/settings/Settings.jsx
@@ -1,41 +1,42 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import Header from "../../Components/header/Header";
+import HeaderMobile from "../../Components/header/HeaderMobile";
 import SettingsCategory from "../../Components/settingsCategory/SettingsCategory";
 import UserDisplay from "../../Components/settingsUser/UserDisplay";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faAngleLeft,faAngleRight} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import "./settings.css";
 
 export default function Settings() {
   const [active, setActive] = useState(false);
   const [showUserDisplay, setShowUserDisplay] = useState(true);
 
-  return(
+  return (
     <>
+      <Header />
       <main className="main-settings">
-        <nav className= {`nav-settings ${active && "is-active"}`}>
+        <nav className={`nav-settings ${active && "is-active"}`}>
           <button className="btn-nav-close" onClick={() => setActive(false)}>
-           <FontAwesomeIcon icon={faAngleLeft} />
+            <FontAwesomeIcon icon={faAngleLeft} />
           </button>
           <div className="nav-container">
             <div className="nav-btn">
-              <button 
-                className="conf-usuarios" 
+              <button
+                className="conf-usuarios"
                 onClick={() => {
-                    setShowUserDisplay(true)
-                    setActive(false)
-                  }
-                }
-                >
+                  setShowUserDisplay(true);
+                  setActive(false);
+                }}
+              >
                 Usuarios
               </button>
-              <button 
-                className="conf-categorias" 
+              <button
+                className="conf-categorias"
                 onClick={() => {
-                    setShowUserDisplay(false)
-                    setActive(false)
-                  }
-                }
-                >
+                  setShowUserDisplay(false);
+                  setActive(false);
+                }}
+              >
                 Categorías
               </button>
             </div>
@@ -44,15 +45,20 @@ export default function Settings() {
         <section className="user-settings">
           {showUserDisplay ? (
             <article className="user-settings-container">
-              <button className="user-btn-nav btn-nav-flex" onClick={() => setActive(true)}>
+              <button
+                className="user-btn-nav btn-nav-flex"
+                onClick={() => setActive(true)}
+              >
                 <FontAwesomeIcon icon={faAngleRight} />
               </button>
               <UserDisplay />
             </article>
-            
           ) : (
             <article className="category-settings-container">
-              <button className="category-btn-nav btn-nav-flex" onClick={() => setActive(true)}>
+              <button
+                className="category-btn-nav btn-nav-flex"
+                onClick={() => setActive(true)}
+              >
                 <FontAwesomeIcon icon={faAngleRight} />
               </button>
               <SettingsCategory />
@@ -60,6 +66,8 @@ export default function Settings() {
           )}
         </section>
       </main>
+
+      <HeaderMobile />
     </>
   );
 }
